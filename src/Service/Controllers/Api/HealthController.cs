88f2019604f891f48d6ef958b57da734cdb0c166
Virágh, Tibor <25325912+poseen@users.noreply.github.com>
2018-11-06 03:19:10 +0100

    [Code]Removing debug thread sleep from health controller.

--- src/Service/Controllers/Api/HealthController.cs
+++ src/Service/Controllers/Api/HealthController.cs
@@ -38,7 +38,7 @@ namespace BoardGame.Service.Controllers.Api
                                    .Default
                                    .Application
                                    .ApplicationVersion;
-            Thread.Sleep(5000);
+
             _logger.LogInformation($"{GetCurrentUserNameOrAnonymous()} has queried the health status.");
             return Ok(version);
         }
