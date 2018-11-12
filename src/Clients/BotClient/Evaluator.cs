89f46701d617d78650823a464ee76c9734a38c77
Virágh, Tibor <25325912+poseen@users.noreply.github.com>
2018-11-12 03:53:22 +0100

    [Code]New evaluator brough to bot client.

--- src/Clients/BotClient/Evaluator.cs
+++ src/Clients/BotClient/Evaluator.cs
@@ -16,19 +16,19 @@ namespace BotClient
             _mechanism = mechanism;
         }
 
-        private ChessPlayer GetOpponent(ChessRepresentation representation)
+        private ChessPlayer GetOpponent(ChessRepresentation state)
         {
-            return representation.CurrentPlayer == ChessPlayer.Black ? ChessPlayer.White : ChessPlayer.Black;
+            return state.CurrentPlayer == ChessPlayer.Black ? ChessPlayer.White : ChessPlayer.Black;
         }
 
         private static readonly Dictionary<PieceKind, int> ChessPieceValues = new Dictionary<PieceKind, int>()
         {
             [PieceKind.Pawn] = 1,
-            [PieceKind.Knight] = 3,
-            [PieceKind.Bishop] = 3,
-            [PieceKind.Rook] = 5,
-            [PieceKind.Queen] = 10,
-            [PieceKind.King] = 4,
+            [PieceKind.Knight] = 6,
+            [PieceKind.Bishop] = 6,
+            [PieceKind.Rook] = 10,
+            [PieceKind.Queen] = 20,
+            [PieceKind.King] = 8,
         };
 
         public int Evaluate(ChessRepresentation state)
@@ -42,40 +42,49 @@ namespace BotClient
 
                 case GameState.BlackWon:
                     return state.CurrentPlayer == ChessPlayer.Black ? int.MaxValue : int.MinValue;
-
-                case GameState.Draw:
-                    return 0;
             }
 
-            var result = GetValueOfThreatenedPositions(state)
-                         + GetRelativeValueOfPiecesOnTable(state)
-                         + GetValueOfCheckStatus(state)
-                         + GetMobilityValue(state);
+            var threatenedValue = GetValueOfThreatenedPositions(state);
+            var piecesValue = GetRelativeValueOfPiecesOnTable(state);
+            var checkStatusValue = GetValueOfCheckStatus(state);
+            var mobilityValue = GetMobilityValue(state);
+            var castlingValue = GetValueForCastling(state);
+
+            if (gameOutcome == GameState.Draw)
+            {
+                if (checkStatusValue > 0 || mobilityValue < 10 || piecesValue > 40)
+                {
+                    return int.MinValue;
+                }
+
+                return -100;
+            }
+
+            var result = threatenedValue
+                         + piecesValue
+                         + checkStatusValue
+                         + mobilityValue
+                         + castlingValue;
 
             return result;
         }
 
-        private int GetValueOfCheckStatus(ChessRepresentation representation)
+        private int GetValueOfCheckStatus(ChessRepresentation state)
         {
-            var inCheck = _mechanism.IsPlayerInChess(representation, representation.CurrentPlayer);
+            var inCheck = _mechanism.IsPlayerInChess(state, state.CurrentPlayer);
             return inCheck ? -1000 : 1000;
         }
 
-        private int GetValueOfThreatenedPositions(ChessRepresentation representation)
+        private int GetValueOfThreatenedPositions(ChessRepresentation state)
         {
-            var opponent = GetOpponent(representation);
+            var opponent = GetOpponent(state);
 
-            var threatenedPositions = _mechanism.GetThreatenedPositions(representation, representation.CurrentPlayer)
-                                          .Select(x => representation[x]?.Kind)
+            var threatenedPositions = _mechanism.GetThreatenedPositions(state, opponent)
+                                          .Select(x => state[x]?.Kind)
                                           .Select(x => x == null ? 1 : ChessPieceValues[x.Value])
-                                          .Sum() * 0.5;
+                                          .Sum() * 0.75;
 
-            var opponentThreatenedPositions = _mechanism.GetThreatenedPositions(representation, opponent)
-                                                  .Select(x => representation[x]?.Kind)
-                                                  .Select(x => x == null ? 1 : ChessPieceValues[x.Value])
-                                                  .Sum() * 0.5;
-
-            return (int)Math.Round(threatenedPositions - opponentThreatenedPositions);
+            return (int)Math.Round(threatenedPositions);
         }
 
         private int GetRelativeValueOfPiecesOnTable(ChessRepresentation state)
@@ -86,18 +95,53 @@ namespace BotClient
                 .Select(x => ChessPieceValues[x.Kind])
                 .Sum();
 
-            var opponentChessPiecesValue = Positions.PositionList.Select(x => state[x])
-                .Where(x => x != null)
-                .Where(x => x.Owner != state.CurrentPlayer)
-                .Select(x => ChessPieceValues[x.Kind])
-                .Sum();
-
-            return chessPiecesValue - opponentChessPiecesValue;
+            return chessPiecesValue;
         }
 
         private int GetMobilityValue(ChessRepresentation state)
         {
-            return + _mechanism.GenerateMoves(state).Count();
+            return _mechanism.GenerateMoves(state).Count();
+        }
+
+        private int GetValueForCastling(ChessRepresentation state)
+        {
+            // Check if the state seems to be after castling...
+
+            var expectedRow = state.CurrentPlayer == ChessPlayer.White ? 1 : 8;
+
+            var kingPosition = Positions.PositionList
+                .Select(x => new { Position = x, Piece = state[x] })
+                .Where(x => x.Piece != null)
+                .Where(x => x.Piece.Owner == state.CurrentPlayer)
+                .Where(x => x.Position.Row == expectedRow)
+                .Where(x => x.Piece.Kind == PieceKind.King)
+                .Select(x => x.Position)
+                .FirstOrDefault();
+
+            var rookPosition = Positions.PositionList
+                .Select(x => new { Position = x, Piece = state[x] })
+                .Where(x => x.Piece != null)
+                .Where(x => x.Piece.Owner == state.CurrentPlayer)
+                .Where(x => x.Position.Row == expectedRow)
+                .Where(x => x.Piece.Kind == PieceKind.Rook)
+                .ToArray();
+
+            if (kingPosition == null || !rookPosition.Any())
+            {
+                return 0;
+            }
+
+            if (kingPosition.Column == 'G' && rookPosition.Any(x => x.Position.Column == 'F'))
+            {
+                return 200;
+            }
+
+            if (kingPosition.Column == 'C' && rookPosition.Any(x => x.Position.Column == 'D'))
+            {
+                return 200;
+            }
+
+            return 0;
         }
     }
 }
